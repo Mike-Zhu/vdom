f57bfae7a8bd3b20f6087e7b516cd3036ee9f885
CN1\zhuzf <zhuzf@ctrip.com>
2018-08-01 10:22:38 +0800

    单个配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -8,68 +8,91 @@ const srcDir = path.join(__dirname, './src');
 const distDir = path.join(__dirname, './dist');
 
 module.exports = {
-  entry: {
-      index: [
-          'webpack/hot/dev-server',
-          'webpack-dev-server/client?http://localhost:3333',
-          "./src/index.js"
-      ]
-  },
-  output: {
-      path: path.resolve(__dirname, 'dist'),
-      // 给js css 图片等在html引入中添加前缀
-      publicPath: "../../",
-      filename: 'js/[name].min.js',
-  },
+    mode: "development",
+    entry: {
+        index: [
+            'webpack/hot/dev-server',
+            'webpack-dev-server/client?http://localhost:8080',
+            "./src/index.js"
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        // 给js css 图片等在html引入中添加前缀
+        publicPath: "../../",
+        filename: 'js/[name].min.js',
+    },
 
-  devtool: 'source-map',
+    devtool: 'source-map',
 
-  module: {
-      rules: [
-          {
-              test: /\.html$/,
-              use: [
-                  {
-                      loader: "html-loader",
-                      options: { minimize: true }
-                  }
-              ]
-          },
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: "babel-loader"
-              }
-          },
-          {
-              test: /\.css$/,
-              exclude: /node_modules/,
-              use: ExtractTextPlugin.extract({
-                  fallback: "style-loader",
-                  use: {
-                      loader: 'css-loader'
-                  },
-              })
-          },
-          {
-              test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-              loader: 'url-loader?limit=8192&name=img/[name].[ext]'
-          }
-      ]
-  },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: {
+                        loader: 'css-loader'
+                    },
+                })
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=8192&name=img/[name].[ext]'
+            }
+        ]
+    },
 
-  plugins: [
-      new CleanWebpackPlugin(['dist']),
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
 
-      new ExtractTextPlugin('style/[name].min.css'),
-      new HtmlWebpackPlugin({
-          hash: true,
-          chunks: ['index'],
-          template: "./src/index.html",
-          filename: "index.html"
-      }),
-      
-      new webpack.HotModuleReplacementPlugin(),
-  ]
+        new ExtractTextPlugin('style/[name].min.css'),
+        new HtmlWebpackPlugin({
+            hash: true,
+            chunks: ['index'],
+            template: "./src/index.html",
+            filename: "index.html"
+        }),
+
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        port: 8080,
+        contentBase: __dirname + '/src/',
+
+        hot: true,
+        historyApiFallback: true,
+        // It suppress error shown in console, so it has to be set to false.
+        quiet: false,
+        // It suppress everything except error, so it has to be set to false as well
+        // to see success build.
+        noInfo: false,
+        stats: {
+            // Config for minimal console.log mess.
+            assets: false,
+            colors: true,
+            version: false,
+            hash: false,
+            timings: false,
+            chunks: false,
+            chunkModules: false
+        }
+    }
 };
